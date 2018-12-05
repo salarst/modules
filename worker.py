64728de28778038df93e12f1b71d8bb0f59b4912
salarst <18659355203@163.com>
2018-12-05 09:38:00 +0800

    fix bugs and add descriptions
    1.replace stdout.read() with stdout to increase the memory usage
    2.add description for every function
    3.fix bugs

--- worker.py
+++ worker.py
@@ -7,28 +7,38 @@ class worker():
 		pass
 	
 	def wait_for_finish(self, stdout):
+		'''
+		wait for paramiko.SSHClient excute command finish
+		'''
 		while not stdout.channel.exit_status_ready():
 			time.sleep(0.5)
 	
-	def do_work_local(self,cmd,isReturncode=False,isReturnStdout=False,isReturnStderr=False):
+	def do_work_local(self,cmdList,returncode=False,returnStdout=False,returnStderr=False):
+		'''
+		execute linux commands with subprocess module and if return* is Ture
+		return the reqiurement values.
+		'''
 		res = {}
 		pipe = subprocess.PIPE
-		p = subprocess.Popen(cmd,stdout = pipe,stderr = pipe,shell=True)
+		p = subprocess.Popen(cmdList,stdout = pipe,stderr = pipe)
 		p.wait()
 		if isReturncode:
 			res['returncode'] = p.returncode
 		if isReturnStdout:
-			res['stdout'] = p.stdout.read()
+			res['stdout'] = p.stdout
 		if isReturnStderr:
 			res['stderr'] = p.stderr.read()
 		return res
 	
-	def do_work_remote(self,ssh,cmd,isReturnStdout=False,isReturnExitStauts=False,isReturnStderr=False):
+	def do_work_remote(self,ssh,cmd,returnStdout=False,returnExitStauts=False,returnStderr=False):
+		'''
+		use paramiko to excute command on remote server and return the results of excution. 
+		'''
 		res = {}
 		stdin,stdout,stderr = ssh.exec_command(cmd)
 		self.wait_for_finish(stdout)
 		if isReturnStdout:
-			res['stdout'] = stdout.read()
+			res['stdout'] = stdout
 		if isReturnExitStauts:
 			res['exitstatus'] = stdout.channel.recv_exit_status()
 		if isReturnStderr:
@@ -36,6 +46,10 @@ class worker():
 		return res
 	
 	def check_service_status(self,ssh,cmd,serviceName):
+		'''
+		check service status.you can use commands like 'netstat -tlnp | grep $port' 
+		or 'systemctl status $service' to check the service status
+		'''
 		stdin, stdout, stderr = ssh.exec_command('%s'%cmd)
 		if stdout.channel.recv_exit_status() == 0:
 			print(color.color['green'] + '%s start sucessfully!'%serviceName + color.end)
@@ -43,12 +57,18 @@ class worker():
 			print(color.color['red'] + '%s start failed!'%serviceName + color.end)
 	
 	
-	def ssh_handers(self,ip,port=22,isKeyLogin=True,passowrd=None):
+	def ssh_handers(self,ip,port=22,keyLogin=True,passowrd=None):
+		'''
+		ssh login to linux server with key or password.
+		if keyLogin is True,use ssh key login,else use password.
+		if password is none,will raise an exception
+		'''
 		if isKeyLogin:
 			pkey = paramiko.RSAKey.from_private_key_file('/root/.ssh/id_rsa')
 		ssh = paramiko.SSHClient()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 		try:
-			if passowrd:
+			if not passowrd:
 				ssh.connect(hostname=ip,port=port,username='root',pkey=pkey)
 			else:
 				ssh.connect(hostname=ip,port=port,username='root',passowrd=passowrd)
